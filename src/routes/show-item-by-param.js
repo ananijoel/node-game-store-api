b6dd5cc0406eb4fb797547ef52eec277b21e788e
ananijoel <ananijoellanatide@gmail.com>
2024-07-15 18:13:49 +0000

    feat:recherche d'une liste d'items par le nom de sa categorie

--- src/routes/show-item-by-param.js
+++ src/routes/show-item-by-param.js
@@ -1,20 +1,32 @@
 const { Item } = require('../dataBase/sequelize');
+const { Op } = require('sequelize')
 
 module.exports = (app) => {
-    app.get('/api/Item/category/:price', (req, res) => {
-        Item.findAll({ where: { price: req.params.price } })
-        .then(items => {
-            if (items.length === 0) {
-                const message = `Aucun item avec le prix demandé n'est disponible.`;
-                return res.status(404).json({ message });
-            }
-            const message = `Des items ont bien été trouvés.`;
-            res.json({ message, data: items });
+    app.get('/api/items/category/:limit/:category', (req, res) => {
+        const category = req.params.category;  // Récupère la catégorie depuis l'URL
+        const limit = parseInt(req.params.limit)
+        Item.findAll({ 
+            where: { 
+                category:{
+                    [Op.or]:{
+                        [Op.like]: `%${category}%`,
+                    }
+                } 
+            },
+            order: ['name'],
+            limit: limit
         })
-        .catch(error => {
-            const message = `Les items n'ont pas pu être récupérés. Réessayez dans quelques instants.`;
-            res.status(500).json({ message, data: error });
-        });
-        console.log( req.params.price )
+            .then(items => {
+                if (items.length === 0) {
+                    const message = `Aucun item trouvé pour la catégorie '${category}'.`;
+                    return res.status(404).json({ message });
+                }
+                const message = `Des items ont bien été trouvés pour la catégorie '${category}'.`;
+                res.json({ message, data: items });
+            })
+            .catch(error => {
+                const message = `Les items n'ont pas pu être récupérés pour la catégorie '${category}'. Réessayez dans quelques instants.`;
+                res.status(500).json({ message, error });
+            });
     });
 };
